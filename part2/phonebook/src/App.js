957742fa61fcf8f3399a49e60943100006be576d
oabrivard <olivier@abrivard.fr>
2022-03-27 22:35:27 -0400

    Implement part 2.c exercises

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import personService from './services/persons'
 
 const Filter = ({ filter, onFilterChange }) => <div>
   filter shown with <input value={filter} onChange={onFilterChange} />
 </div>
 
-const PersonForm = ({newName, onNameChange, newNumber, onNumberChange, onSubmitClick}) => <form>
+const PersonForm = ({ newName, onNameChange, newNumber, onNumberChange, onSubmitClick }) => <form>
   <div>
     name: <input value={newName} onChange={onNameChange} />
   </div>
@@ -16,25 +17,31 @@ const PersonForm = ({newName, onNameChange, newNumber, onNumberChange, onSubmitC
   </div>
 </form>
 
-const Person = ({ person }) => <div>
-  {person.name} {person.number}
+const Person = ({ person, onPersonDelete }) => <div>
+  {person.name} {person.number} <button onClick={onPersonDelete}>delete</button>
 </div>
 
-const Persons = ({ persons, filter }) => <div>
-  {persons.filter(p => p.name.toUpperCase().includes(filter.toUpperCase())).map(p => <Person key={p.name} person={p} />)}
-</div>
+const Persons = ({ persons, filter, deletePerson }) =>
+  <div>
+    {persons
+      .filter(p => p.name.toUpperCase().includes(filter.toUpperCase()))
+      .map(p => <Person key={p.id} person={p} onPersonDelete={() => deletePerson(p.id, p.name)} />)}
+  </div>
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [filter, setFilter] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(allPersons => {
+        setPersons(allPersons)
+      })
+  }, [])
 
   const onFilterChange = (event) => {
     setFilter(event.target.value)
@@ -50,12 +57,32 @@ const App = () => {
 
   const onSubmitClick = (event) => {
     event.preventDefault()
-    if (persons.find(p => p.name == newName)) {
-      alert(`${newName} is already added to phonebook`)
+    const existingPerson = persons.find(p => p.name == newName)
+    if (existingPerson) {
+      if (window.confirm(`${existingPerson.name} is already added to phonebook, replace the old number with a new one?`)) {
+        const updatedPerson = { ...existingPerson, number: newNumber }
+        personService.update(existingPerson.id, updatedPerson)
+        .then(person => {
+          setPersons(persons.map(p => p.id !== person.id ? p : person))
+        })
+      }
     } else {
-      setPersons(persons.concat({ name: newName, number: newNumber }))
-      setNewName('')
-      setNewNumber('')
+      const newPerson = { name: newName, number: newNumber }
+      personService
+        .create(newPerson)
+        .then(person => {
+          setPersons(persons.concat(person))
+          setNewName('')
+          setNewNumber('')
+        })
+    }
+  }
+
+  const deletePerson = (id, name) => {
+    if (window.confirm(`Delete ${name} ?`)) {
+      personService
+        .remove(id)
+        .then(() => setPersons(persons.filter(p => p.id !== id)))
     }
   }
 
@@ -66,7 +93,7 @@ const App = () => {
       <h2>add a new</h2>
       <PersonForm newName={newName} onNameChange={onNameChange} newNumber={newNumber} onNumberChange={onNumberChange} onSubmitClick={onSubmitClick} />
       <h2>Numbers</h2>
-      <Persons persons={persons} filter={filter} />
+      <Persons persons={persons} filter={filter} deletePerson={deletePerson} />
     </div>
   )
 }
---
+++ part2/phonebook/src/services/persons.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/persons'
+
+const getAll = () => {
+  return axios.get(baseUrl).then(response => response.data)
+}
+
+const create = (newObject) => {
+  return axios.post(baseUrl, newObject).then(response => response.data)
+}
+
+const update = (id, newObject) => {
+  return axios.put(`${baseUrl}/${id}`, newObject).then(response => response.data)
+}
+
+const remove = (id) => {
+  return axios.delete(`${baseUrl}/${id}`)
+}
+
+export default { getAll, create, update, remove }
