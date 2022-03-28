1f1f2c863b022bc58848c5b78d588a4e3981cf2d
oabrivard <olivier@abrivard.fr>
2022-03-27 23:31:15 -0400

    Finish part 2 exercises

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,6 +1,28 @@
 import { useState, useEffect } from 'react'
 import personService from './services/persons'
 
+const Notification = ({ notification }) => {
+  if (notification === null) {
+    return null
+  }
+
+  const notificationStyle = {
+    color: notification.color,
+    background: 'lightgrey',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10
+  }
+
+  return (
+    <div style={notificationStyle}>
+      {notification.message}
+    </div>
+  )
+}
+
 const Filter = ({ filter, onFilterChange }) => <div>
   filter shown with <input value={filter} onChange={onFilterChange} />
 </div>
@@ -34,6 +56,7 @@ const App = () => {
   const [filter, setFilter] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [notification, setNotification] = useState(null)
 
   useEffect(() => {
     personService
@@ -42,6 +65,13 @@ const App = () => {
         setPersons(allPersons)
       })
   }, [])
+
+  const displayNotification = (newNotification) => {
+    setNotification(newNotification)
+    setTimeout(() => {
+      setNotification(null)
+    }, 5000)
+  }
 
   const onFilterChange = (event) => {
     setFilter(event.target.value)
@@ -57,13 +87,18 @@ const App = () => {
 
   const onSubmitClick = (event) => {
     event.preventDefault()
-    const existingPerson = persons.find(p => p.name == newName)
+    const existingPerson = persons.find(p => p.name === newName)
     if (existingPerson) {
       if (window.confirm(`${existingPerson.name} is already added to phonebook, replace the old number with a new one?`)) {
         const updatedPerson = { ...existingPerson, number: newNumber }
         personService.update(existingPerson.id, updatedPerson)
         .then(person => {
+          displayNotification({color:'green', message:`Updated '${existingPerson.name}'`})
           setPersons(persons.map(p => p.id !== person.id ? p : person))
+        })
+        .catch(error => {
+          displayNotification({color:'red', message:`the person '${existingPerson.name}' was already deleted from server`})
+          setPersons(persons.filter(p => p.id !== existingPerson.id))
         })
       }
     } else {
@@ -71,6 +106,7 @@ const App = () => {
       personService
         .create(newPerson)
         .then(person => {
+          displayNotification({color:'green', message:`Added '${newPerson.name}'`})
           setPersons(persons.concat(person))
           setNewName('')
           setNewNumber('')
@@ -82,13 +118,21 @@ const App = () => {
     if (window.confirm(`Delete ${name} ?`)) {
       personService
         .remove(id)
-        .then(() => setPersons(persons.filter(p => p.id !== id)))
+        .then(() => {
+          displayNotification({color:'green', message:`Deleted '${name}'`})
+          setPersons(persons.filter(p => p.id !== id))
+        })
+        .catch(error => {
+          displayNotification({color:'red', message:`the person '${name}' was already deleted from server`})
+          setPersons(persons.filter(p => p.id !== id))
+        })
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification notification={notification} />
       <Filter value={filter} onFilterChange={onFilterChange} />
       <h2>add a new</h2>
       <PersonForm newName={newName} onNameChange={onNameChange} newNumber={newNumber} onNumberChange={onNumberChange} onSubmitClick={onSubmitClick} />
