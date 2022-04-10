5c676829207be923dbf86f702181487d350f83ca
oabrivard <olivier@abrivard.fr>
2022-04-10 10:28:32 -0400

    Implement part 3.c exercises

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
-app.use(express.json())
 app.use(express.static('build'))
+app.use(express.json())
 
 const cors = require('cors')
 app.use(cors())
@@ -10,25 +12,27 @@ const morgan = require('morgan')
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body'))
 
+const Person = require('./models/person')
+
 let persons = [
-  { 
+  {
     "id": 1,
-    "name": "Arto Hellas", 
+    "name": "Arto Hellas",
     "number": "040-123456"
   },
-  { 
+  {
     "id": 2,
-    "name": "Ada Lovelace", 
+    "name": "Ada Lovelace",
     "number": "39-44-5323523"
   },
-  { 
+  {
     "id": 3,
-    "name": "Dan Abramov", 
+    "name": "Dan Abramov",
     "number": "12-43-234345"
   },
-  { 
+  {
     "id": 4,
-    "name": "Mary Poppendieck", 
+    "name": "Mary Poppendieck",
     "number": "39-23-6423122"
   }
 ]
@@ -38,58 +42,98 @@ app.get('/', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-  response.set('Content-Type', 'text/html')
-  response.write(`<p>Phonebook has info for ${persons.length}  ${persons.length===1 ? "person" : "people"}</p>`)
-  response.write(`<p>${new Date()}</p>`)
-  response.end()
+  Person.countDocuments({}).then(count => {
+    response.set('Content-Type', 'text/html')
+    response.write(`<p>Phonebook has info for ${count}  ${count === 1 ? "person" : "people"}</p>`)
+    response.write(`<p>${new Date()}</p>`)
+    response.end()
+  })
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(result => {
+    response.json(result)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(p => p.id === id)
-  
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).send("Person not found")
-  }
+  Person.findById(request.params.id).then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).send("Person not found")
+    }
+  }).catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(p => p.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
   if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'name or number missing' 
+    return response.status(400).json({
+      error: 'name or number missing'
     })
-  } 
-
-  if (persons.find(p => p.name === body.name)) {
-    return response.status(400).json({ 
-      error: 'name already exists' 
-    })
-  } 
-
-  const person = {
-    ...body,
-    id: Math.floor(Math.random() * 999999),
   }
 
-  persons = persons.concat(person)
+  const person = new Person({ ...body })
 
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+  // if (persons.find(p => p.name === body.name)) {
+  //   return response.status(400).json({
+  //     error: 'name already exists'
+  //   })
+  // }
+
+  // const person = {
+  //   ...body,
+  //   id: Math.floor(Math.random() * 999999),
+  // }
+
+  // persons = persons.concat(person)
+
+  // response.json(person)
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  Person.findByIdAndUpdate(request.params.id, body, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://oabrivard:${password}@fullstackopen.6kr6n.mongodb.net/phonebook-fsopen2022?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 5) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  })
+
+  person.save().then(result => {
+    console.log('person saved!')
+    mongoose.connection.close()
+  })
+} else if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    result.forEach(p => {
+      console.log(p)
+    })
+    mongoose.connection.close()
+  })
+} else {
+  console.log('Person name and number are mandatory')
+  mongoose.connection.close()
+  process.exit(1)
+}
